bef3136d16b697c7a51d4bb422fa987ea19fd270
Joe <mrjoeroddy@gmail.com>
2019-01-29 17:41:33 -0500

    firestore update tests

--- src/query_runners/test_resources/setup_db.js
+++ src/query_runners/test_resources/setup_db.js
@@ -1,5 +1,7 @@
 import * as admin from "firebase-admin";
 import config from "./db_config";
+import executeQuery from "../../execute";
+import { configureFbsql } from "../..";
 
 const { databaseURL, serviceAccount } = config;
 
@@ -8,16 +10,47 @@ admin.initializeApp({
   databaseURL
 });
 
-export const clearDb = () => {
-  return admin
-    .database()
-    .ref("/")
-    .set(null);
+export const clearDb = async isFirestore => {
+  return isFirestore
+    ? deleteFirestore()
+    : admin
+        .database()
+        .ref("/")
+        .set(null);
 };
 
-export const injectData = (path, data) => {
-  return admin
-    .database()
-    .ref(path)
-    .set(data);
+export const injectData = (path, data, isFirestore) => {
+  return isFirestore
+    ? injectIntoFirestore(path, data)
+    : admin
+        .database()
+        .ref(path)
+        .set(data);
+};
+
+const injectIntoFirestore = (path, data) => {
+  const db = admin.firestore();
+  const batch = db.batch();
+
+  data &&
+    Object.keys(data).forEach(docTitle => {
+      batch.set(db.collection(path).doc(docTitle), data[docTitle]);
+    });
+  return batch.commit();
+};
+
+const deleteFirestore = async () => {
+  configureFbsql({ isFirestore: true });
+  const rootData = await executeQuery("select * from /");
+  const db = admin.firestore();
+  const batch = db.batch();
+  rootData &&
+    Object.keys(rootData).forEach(colKey => {
+      const collectionData = rootData[colKey];
+      collectionData &&
+        Object.keys(collectionData).forEach(docId => {
+          batch.delete(db.collection(colKey).doc(docId));
+        });
+    });
+  return batch.commit();
 };
